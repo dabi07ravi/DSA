f7d80d0bc4fa6c36e85854cbf7df3834b7f3e4fb
Ravindra Singh Dabi <raghav.dabi.7@gmail.com>
2026-01-22 15:33:10 +0530

    add optimal sol for subarraywithgivensumk

--- subarraywithgivensum.js
+++ subarraywithgivensum.js
@@ -4,6 +4,7 @@ let sum = 0
 let count = 0
 
 
+// brute force
 for (i = 0; i < arr.length; i++) {
     for (j = i; j < arr.length; j++) {
         sum = sum + arr[j]
@@ -16,4 +17,43 @@ for (i = 0; i < arr.length; i++) {
 
     sum = 0
 } 
+
+
+
+// optimal sol sliding window
+// function subArrayWithGivenSum(a, t) {
+//   let subArrayIndex = [];
+
+//   //   for (i = 0; i < a.length; i++) {
+//   //     let sum = 0;
+//   //     for (j = i; j < a.length; j++) {
+//   //       sum = sum + a[j];
+//   //       if (sum === t) {
+//   //         subArrayIndex.push(i, j);
+//   //       }
+//   //     }
+//   //   }
+
+//   let left = 0;
+//   let right = 0;
+//       let sum = 0;
+
+
+//   for (right; right < a.length; right++) {
+//     sum = sum + a[right];
+
+//     while (sum > t) {
+//       sum = sum - a[left];
+//       left++;
+//     }
+//     if (sum === t) {
+//       subArrayIndex.push(left, right);
+//       break;
+//     }
+//   }
+
+//   return subArrayIndex;
+// }
+
+// console.log(subArrayWithGivenSum([1, 10, 4, 0, 3, 5], 7));
 
