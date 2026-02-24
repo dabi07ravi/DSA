312345f3dfc14093ac3a7eac966139279ac8039d
Ravindra Singh Dabi <raghav.dabi.7@gmail.com>
2026-02-24 11:54:13 +0530

    add extra optimal method in string compression

--- stringCompression.js
+++ stringCompression.js
@@ -1,24 +1,44 @@
-let str = 'aabccccc'
-let res = []
+// let str = 'aabccccc'
+// let res = []
 
 
 
-let map = new Map();
+// let map = new Map();
 
 
 
-for (i=0; i < str.length; i++){
-    let ch = str[i]
-    map.set(ch, (map.get(ch) || 0) + 1);
+// for (i=0; i < str.length; i++){
+//     let ch = str[i]
+//     map.set(ch, (map.get(ch) || 0) + 1);
+// }
+
+
+// map.forEach((values, keys) => {
+
+//     res.push(keys+values)
+// })
+
+
+// console.log(res.join(''))
+
+
+
+let str = "aabccccc";
+let res = "";
+let count = 1;
+
+for (i = 1; i < str.length; i++) {
+  if (str[i] === str[i - 1]) {
+    count++;
+  } else {
+    res = res + str[i - 1] + count;
+    count = 1;
+  }
 }
 
+res = res + str[i - 1] + count;
 
-map.forEach((values, keys) => {
+console.log(res);
 
-    res.push(keys+values)
-})
-
-
-console.log(res.join(''))
 
 
