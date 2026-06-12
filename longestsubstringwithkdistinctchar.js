79789b852a60df8040441456d8aff5d1fdfe71bb
Ravindra Singh Dabi <raghav.dabi.7@gmail.com>
2026-06-12 12:01:37 +0530

    correct the code

--- longestsubstringwithkdistinctchar.js
+++ longestsubstringwithkdistinctchar.js
@@ -1,80 +1,41 @@
-// function longestsubstringwithkdistinctchar(s, k) {
-//   let fmap = new Map();
+function longestSubstringWithKDistinctChars(s, k) {
+    let fmap = new Map();
 
-//   let left = 0;
-//   let right = 0;
-//   let maxLen = 0;
-//   let startIndex = 0;
+    let left = 0;
+    let maxLen = 0;
+    let startIndex = 0;
 
-//   for (right; right < s.length; right++) {
-//     let rightChar = s[right];
-//     fmap.set(rightChar, (fmap.get(rightChar) || 0) + 1);
+    for (let right = 0; right < s.length; right++) {
+        let rightChar = s[right];
 
-//     if (fmap.size <= k) {
-//       let len = right - left + 1;
-//       if (len > maxLen) {
-//         maxLen = len;
-//         startIndex = left;
-//       }
-//     } else {
-//       while (fmap.size > k) {
-//         let leftChar = s[left];
-//         if (fmap.has(leftChar)) {
-//           fmap.set(leftChar, fmap.get(leftChar) - 1);
-//         }
-//         if (fmap.has(leftChar) && fmap.get(leftChar) === 0) {
-//           fmap.delete(leftChar);
-//         }
-//         left++;
-//       }
-//     }
-//   }
+        fmap.set(rightChar, (fmap.get(rightChar) || 0) + 1);
 
-//   return {maxLen, str: s.slice(startIndex,startIndex+maxLen)}
-// }
+        // Shrink until window becomes valid
+        while (fmap.size > k) {
+            let leftChar = s[left];
 
-// // Example
-// console.log(longestsubstringwithkdistinctchar("aaabbccd", 2));
+            fmap.set(leftChar, fmap.get(leftChar) - 1);
 
+            if (fmap.get(leftChar) === 0) {
+                fmap.delete(leftChar);
+            }
 
+            left++;
+        }
 
+        // Window is guaranteed valid here
+        let len = right - left + 1;
 
-let a = "aaabbccd";
-let k = 2;
-
-let fmap = new Map();
-
-let left = 0;
-let right = 0;
-let maxLen = -Infinity;
-
-
-for (right; right < a.length; right++) {
-  let rightChar = a[right];
-
-  fmap.set(rightChar, (fmap.get(rightChar) || 0) + 1);
-
-  if (fmap.size > k) {
-    let leftChar = a[left];
-    if (fmap.has(leftChar)) {
-      fmap.set(leftChar, fmap.get(leftChar) - 1);
-
-      if (fmap.get(leftChar) === 0) {
-        fmap.delete(leftChar);
-      }
+        if (len > maxLen) {
+            maxLen = len;
+            startIndex = left;
+        }
     }
 
-    left++
-  }
-
-  if(fmap.size <= k){
-        let len = right-left+1
-        if(len > maxLen){
-            maxLen = len
-        }
-  }
+    return {
+        maxLen,
+        str: s.slice(startIndex, startIndex + maxLen)
+    };
 }
 
-
-console.log(maxLen)
-
+console.log(longestSubstringWithKDistinctChars("aaabbccd", 2));
